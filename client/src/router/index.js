725b3d522c1dd0eb88d6fb82ab9ece15c6360528
Gitari65 <agitari65@gmail.com>
2024-05-23 16:19:38 +0300

    routing and layout configurations

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,16 +1,36 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginPage from '../components/LoginPage.vue';
+import AddEvent from '../components/AddEvent.vue';
+import RegisterPage from '../components/RegisterPage.vue';
+import MainLayout from '../components/MainLayout.vue'; // New layout component
+import AuthLayout from '../components/AuthLayout.vue';
 
-import LoginPage from '../components/LoginPage.vue'
-import AddEvent from '../components/AddEvent.vue'
-import { createRouter,createWebHistory } from 'vue-router';
+const routes = [
+  {
+    path: '/',
+    component: AuthLayout,
+    children: [
+      { path: '', name: 'login', component: LoginPage },
+      { path: 'register', name: 'register', component: RegisterPage },
+    ],
+  },
+  {
+    path: '/',
+    name: 'main',
+    components: {
+      default: MainLayout,
+    },
+    children: [
+      
+      { path: 'event/create', name: 'addevent', component: AddEvent },
+   
+    ],
+  },
+];
 
-
-
-const router=createRouter({
-    history:createWebHistory(),
-    routes:[
-        {path:'/',name:"login",component:LoginPage},
-        {path:'/event/create',name:"addevent",component:AddEvent}
-    ]
-})
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
 
 export default router;
