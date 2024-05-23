0c48e25d12facc4ce4b01d3401e371ca32e7f58b
Gitari65 <agitari65@gmail.com>
2024-05-23 16:19:19 +0300

    routing and layout configurations

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,33 @@
 <template>
- 
-<router-view></router-view>
+  <div class="main-container d-flex">
+    <router-view name="sidebar" />
+    <div class="content-container">
+      <router-view name="navbar" />
+      <div class="content">
+        <router-view />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import '../src/assets/styles/styles.css'
-
-
+import  '../src/assets/styles/styles.css'
 export default {
   name: 'App',
- 
-
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.main-container {
+  display: flex;
+}
+.content-container {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+}
+.content {
+  flex-grow: 1;
+  padding: 20px;
 }
 </style>
