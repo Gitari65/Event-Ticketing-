b0e9880cd073ce0a1974829a0bf9ac7ae2828b09
Gitari65 <agitari65@gmail.com>
2024-05-29 11:28:32 +0300

    creating ticket model

--- server/app/Models/Event.php
+++ server/app/Models/Event.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+;
 
 class Event extends Model
 {
@@ -14,4 +15,7 @@ class Event extends Model
         'location',
         'user_id'
     ];
+    public function tickets(){
+        return $this->hasMany(Ticket::class);
+    }
 }
