8d8e6cdfb08295f6e68f88c3d46291a315f8557b
Gitari65 <agitari65@gmail.com>
2024-05-31 15:50:30 +0300

    frontend  for a creating and viewing tickets

--- client/src/assets/constants/ErrorPage.vue
+++ client/src/assets/constants/ErrorPage.vue
@@ -1,59 +1,51 @@
 <template>
-    <div class="error">
-      <div class="error__icon">
-        <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-          <path clip-rule="evenodd" d="m12 1c-6.075 0-11 4.925-11 11s4.925 11 11 11 11-4.925 11-11-4.925-11-11-11zm4.768 9.14c.0878-.1004.1546-.21726.1966-.34383.0419-.12657.0581-.26026.0477-.39319-.0105-.13293-.0475-.26242-.1087-.38085-.0613-.11844-.1456-.22342-.2481-.30879-.1024-.08536-.2209-.14938-.3484-.18828s-.2616-.0519-.3942-.03823c-.1327.01366-.2612.05372-.3782.1178-.1169.06409-.2198.15091-.3027.25537l-4.3 5.159-2.225-2.226c-.1886-.1822-.4412-.283-.7034-.2807s-.51301.1075-.69842.2929-.29058.4362-.29285.6984c-.00228.2622.09851.5148.28067.7034l3 3c.0983.0982.2159.1748.3454.2251.1295.0502.2681.0729.4069.0665.1387-.0063.2747-.0414.3991-.1032.1244-.0617.2347-.1487.3236-.2554z" fill="#FFA0A0" fill-rule="evenodd"></path>
-        </svg>
-      </div>
-      <div class="error__title">Error: Lorem ipsum dolor sit amet</div>
-      <div class="error__close">
-        <svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg">
-          <path d="m15.8333 5.34166-1.175-1.175-4.6583 4.65834-4.65833-4.65834-1.175 1.175 4.65833 4.65834-4.65833 4.6583 1.175 1.175 4.65833-4.6583 4.6583 4.6583 1.175-1.175-4.6583-4.6583z" fill="#841A1A"></path>
-        </svg>
-      </div>
-    </div>
-  </template>
-  
-  <style scoped>
-  .error {
-    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    width: 320px;
-    padding: 12px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: start;
-    background: rgba(255, 160, 160, 0.5);
-    border-radius: 8px;
-    box-shadow: 0px 0px 5px -3px #111;
+  <div class="error-page">
+    <div class="icon">&#128543;</div>
+    <div class="message">{{ message }}</div>
+    <button @click="hidePage" class="ok-button">OK</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    hidePage() {
+      this.$emit('hide');
+    }
   }
-  
-  .error__icon {
-    width: 20px;
-    height: 20px;
-    transform: translateY(-2px);
-    margin-right: 8px;
-  }
-  
-  .error__icon path {
-    fill: #FF0000;
-  }
-  
-  .error__title {
-    font-weight: 500;
-    font-size: 14px;
-    color: #841A1A;
-  }
-  
-  .error__close {
-    width: 20px;
-    height: 20px;
-    cursor: pointer;
-    margin-left: auto;
-  }
-  
-  .error__close path {
-    fill: #841A1A;
-  }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.error-page {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #ef9a9a; /* Light red */
+  color: #333;
+  padding: 20px;
+}
+
+.icon {
+  font-size: 48px;
+  margin-right: 20px;
+}
+
+.message {
+  font-size: 20px;
+}
+
+.ok-button {
+  background-color: #fff;
+  color: #333;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+</style>
