5d64e029cd3a5a0bfb77429e286d0bbe0052e525
Gitari65 <agitari65@gmail.com>
2024-06-02 09:05:14 +0300

    frontend- adding router configurations

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from './axios' // Import the axios configuration
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import store from './components/store'
+
 
 // Make Axios available globally
 const app = createApp(App);
@@ -10,6 +14,7 @@ app.config.globalProperties.$axios = axios;
 
 // Use the router
 app.use(router);
+app.use(store);
 
 // Mount the app
 app.mount('#app');
