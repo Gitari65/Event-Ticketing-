50cfd6e0871fd4ff4c43a10149b4f02e6dd1ed5b
Gitari65 <agitari65@gmail.com>
2024-07-11 13:20:29 +0300

    seetting up vuex store for auth tokens

--- client/src/components/store/index.js
+++ client/src/components/store/index.js
@@ -3,20 +3,39 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
-    user: null, // Ensure this matches the structure you expect
+    user: {
+      name: null,
+      email: null,
+      userId:null
+    },
+    intendedRoute: null,
+    // other state properties
   },
   mutations: {
     setUser(state, user) {
       state.user = user;
     },
+    setIntendedRoute(state, route) {
+      state.intendedRoute = route;
+    },
+    LOGOUT(state) {
+      
+      state.user = { name: null, email: null,userId:null }; // reset user to initial state
+    },
   },
   actions: {
-    // Define your actions to commit mutations and fetch user data, if needed
     setUser({ commit }, user) {
       commit('setUser', user);
-    }
+    },
+    logout({ commit }) {
+      commit('LOGOUT');
+      localStorage.removeItem('token'); // clear token
+      localStorage.removeItem('user'); // clear user data
+    },
+    // other actions if needed
   },
   getters: {
     user: (state) => state.user,
+    // other getters if needed
   },
 });
