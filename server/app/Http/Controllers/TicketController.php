e79accf823ff8cfcffb9f3718d1253aede6398f5
Gitari65 <agitari65@gmail.com>
2024-07-11 13:21:57 +0300

    events and tickets available changes

--- server/app/Http/Controllers/TicketController.php
+++ server/app/Http/Controllers/TicketController.php
@@ -7,28 +7,27 @@ use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    //
-    public function storeTickets(Request $request){
-        $ticketData=$request->validate([
-            'eventId'=>['required','numeric'],
-            'tickets'=>['required','array'],
-            'tickets.*.type'=>['required'],
-            'tickets.*.price'=>['required'],
+    public function storeTickets(Request $request)
+    {
+        $ticketData = $request->validate([
+            'eventId' => ['required', 'numeric'],
+            'tickets' => ['required', 'array'],
+            'tickets.*.type' => ['required'],
+            'tickets.*.price' => ['required'],
+            'tickets.*.amount' => ['required'],
         ]);
-       
-        foreach($ticketData['tickets'] as $inputData){
-             Ticket::create([
-               
-                'event_id'=> $ticketData['eventId'],
-                'type'=> $inputData['type'],
-                'price'=> $inputData['price'], 
-             ]
-                        );
+
+        foreach ($ticketData['tickets'] as $inputData) {
+            Ticket::create([
+                'event_id' => $ticketData['eventId'],
+                'type' => $inputData['type'],
+                'price' => $inputData['price'],
+                'amount' => $inputData['amount'] // Ensure 'amount' is provided
+            ]);
         }
 
         return response()->json([
-            'message' => 'Ticket created successfully',
-            
+            'message' => 'Tickets created successfully',
         ], 201);
     }
 }
