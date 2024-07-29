476cbca45c0578f4032d244dbf4fe6fd9b824563
Gitari65 <agitari65@gmail.com>
2024-07-29 21:08:08 +0300

    payments via mpesa

---
+++ server/app/Http/Controllers/MpesaController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\MpesaService;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    protected $mpesaService;
+
+    public function __construct(MpesaService $mpesaService)
+    {
+        $this->mpesaService = $mpesaService;
+    }
+
+    public function initiatePayment(Request $request)
+    {
+        $validatedData = $request->validate([
+            'phone_number' => 'required',
+            'amount' => 'required|numeric',
+        ]);
+
+        $response = $this->mpesaService->lipaNaMpesa(
+            $validatedData['phone_number'],
+            $validatedData['amount'],
+            'EventTickets',
+            'Payment for Event Tickets'
+        );
+
+        return response()->json($response);
+    }
+
+    public function mpesaCallback(Request $request)
+    {
+        Log::info('M-Pesa Callback:', $request->all());
+
+        // Process the callback data here
+    }
+}
